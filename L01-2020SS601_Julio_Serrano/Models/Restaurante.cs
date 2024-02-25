fe5f221890a69b9fb57be813cdf15152692dcab1
jserr <jserr@JC>
2024-02-24 21:12:02 -0600

    clase resturante

---
+++ L01-2020SS601_Julio_Serrano/Models/Restaurante.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2020SS601_Julio_Serrano.Models
+{
+    public class Restaurante
+    {
+        public class Cliente
+        {
+            [Key]
+            public int ClienteId { get; set; }
+            public string NombreCliente { get; set; }
+            public string Direccion { get; set; }
+        }
+
+        public class Pedido
+        {
+            [Key]
+            public int PedidoId { get; set; }
+            public int MotoristaId { get; set; }
+            public int ClienteId { get; set; }
+            public int PlatoId { get; set; }
+            public int Cantidad { get; set; }
+            public decimal Precio { get; set; }
+        }
+
+        public class Plato
+        {
+            [Key]
+            public int PlatoId { get; set; }
+            public string NombrePlato { get; set; }
+            public decimal Precio { get; set; }
+        }
+
+        public class Motorista
+        {
+            [Key]
+            public int MotoristaId { get; set; }
+            public string NombreMotorista { get; set; }
+        }
+    }
+}
